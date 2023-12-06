ac644c1440898fc802dc860756cb0431c4e049c2
Danny Wahyudi <wahyudidanny23@gmail.com>
2023-12-06 10:46:50 +0700

    1. Get Data With Signal R, using connection string

--- SignalR_SqlTableDependecy/Hubs/DashboardHub.cs
+++ SignalR_SqlTableDependecy/Hubs/DashboardHub.cs
@@ -1,9 +1,24 @@
 using Microsoft.AspNetCore.SignalR;
+using SignalR_SqlTableDependecy.Repositories;
 
 namespace SignalR_SqlTableDependecy.Hubs
 {
     public class DashboardHub : Hub
     {
-        
+
+        private readonly ProductRepository _productRepository;
+        private readonly string _connectionString = string.Empty;
+        public DashboardHub(IConfiguration? configuration)
+        {
+            _connectionString = configuration?.GetConnectionString("DefaultConnection") ?? "defaultFallbackConnectionString";
+            _productRepository = new ProductRepository(_connectionString);
+        }
+
+        public async Task SendProducts()
+        {
+            var products = _productRepository.GetProducts();
+            await Clients.All.SendAsync("ReceivedProducts", products);
+        }
+
     }
 }
---
+++ SignalR_SqlTableDependecy/Repositories/ProductRepository.cs
@@ -0,0 +1,78 @@
+using System.Data;
+using System.Data.SqlClient;
+using SignalR_SqlTableDependecy.Models;
+
+namespace SignalR_SqlTableDependecy.Repositories
+{
+    public class ProductRepository
+    {
+
+        private readonly string _connectionString;
+
+        public ProductRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Product> GetProducts()
+        {
+
+            List<Product> products = new List<Product>();
+            Product product;
+
+            var data = GetProductDetailFromDb();
+            foreach (DataRow row in data.Rows)
+            {
+                product = new Product
+                {
+
+
+                    Id = Convert.ToInt32(row["Id"]),
+                    Name = row["Name"].ToString(),
+                    Category = row["Category"].ToString(),
+                    Price = Convert.ToInt32(row["Price"])
+                };
+                products.Add(product);
+            }
+
+            return products;
+
+        }
+
+
+
+        public DataTable GetProductDetailFromDb()
+        {
+            var query = "SELECT Id, Name, Category, Price FROM Product";
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            dataTable.Load(reader);
+                        }
+                    }
+
+                    return dataTable;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+
+
+        }
+    }
+}
